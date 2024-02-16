440add81091936073fbd6ee78753ba9edba6cbb6
amandinhacruz <amandaellem2023@gmail.com>
2024-02-15 21:12:17 -0300

    subindo primeira atv com react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+function Home() {
+    return(
+    <>
+    <h1 className="titulo">Amanda Cruz</h1>
+    <img className="img" src="https://2.bp.blogspot.com/-ldAbyujUy8M/V1emzJAWY0I/AAAAAAAAAuQ/F68N1ptNpJM2wb86ukf-onL-rzv2Z7paQCKgB/s1600/adorable-chaton-qui-a-sommeil.jpg" alt="" />
+    </>
+    )
+}
+
+export default Home;
